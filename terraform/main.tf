cbcbc2c994d18fd4d67aea462dc719e394127469
Chandula Jayathilake <114409648+ChandulaJ@users.noreply.github.com>
2025-08-30 12:43:59 +0530

    link set

--- terraform/main.tf
+++ terraform/main.tf
@@ -39,9 +39,8 @@ resource "aws_instance" "jenkins_build_agent" {
 
   # Install specific older Docker Compose (classic binary)
   DOCKER_COMPOSE_VERSION=1.29.2   # <--- change to your desired version
-  curl -L "https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-$(uname -s)-$(uname -m)" \
-    -o /usr/local/bin/docker-compose
-  chmod +x /usr/local/bin/docker-compose
+curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-Linux-x86_64" -o /usr/local/bin/docker-compose
+ chmod +x /usr/local/bin/docker-compose
 
   # Enable Docker service
   systemctl enable docker
